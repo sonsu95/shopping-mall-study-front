697cb8914e882e31df878a5893ace2a15289e99e
sonsu95 <tjdtn951007@gmail.com>
2024-06-15 21:50:49 +0900

    feat: Navbar 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from '@/app/Proviers';
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from '@nextui-org/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +27,38 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar>
+          <NavbarBrand>
+            <p className="font-bold text-inherit">ACME</p>
+          </NavbarBrand>
+          <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Features
+              </Link>
+            </NavbarItem>
+            <NavbarItem isActive>
+              <Link href="#" aria-current="page">
+                Customers
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link color="foreground" href="#">
+                Integrations
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+          <NavbarContent justify="end">
+            <NavbarItem className="hidden lg:flex">
+              <Link href="#">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button as={Link} color="primary" href="#" variant="flat">
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
         <Providers>{children}</Providers>
       </body>
     </html>
