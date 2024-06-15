e6e12e762faf05765970e0bc856d6552eb5fad53
sonsu95 <tjdtn951007@gmail.com>
2024-06-15 21:50:39 +0900

    feat: 테스트용 api 출력

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,45 @@
+'use client';
+
 import { Button } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
+import { api } from '@/app/_api/api';
+
+interface Item {
+  id: number;
+  itemDetail: string;
+  itemNm: string;
+  price: number;
+  regTime: string;
+  sellStatCd: string;
+  updateTime: string;
+}
 
 export default function Home() {
+  const [item, setItem] = useState<Item | null>(null);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await api.get<Item>('/product/test');
+      return res.data;
+    }
+
+    getData().then((data) => {
+      setItem(data);
+    });
+  }, []);
+
   return (
     <main>
+      <div>
+        {item ? (
+          <>
+            <p>제품명: {item.itemNm}</p>
+            <p>가격: {item.price}원</p>
+          </>
+        ) : (
+          <p>로딩 중...</p>
+        )}
+      </div>
       <Button>버튼 테스트</Button>
     </main>
   );
