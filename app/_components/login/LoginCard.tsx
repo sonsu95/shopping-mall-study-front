62e3ec4456854b736dbb914dfc557114f1f158d7
sonsu95 <tjdtn951007@gmail.com>
2024-06-16 18:23:00 +0900

    feat: 로그인 폼 작업 추가

---
+++ app/_components/login/LoginCard.tsx
@@ -0,0 +1,46 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Input,
+} from '@nextui-org/react';
+import Link from 'next/link';
+
+export default function LoginCard() {
+  return (
+    <Card
+      className="flex w-full max-w-[30rem] justify-center px-10 py-5"
+      radius="sm"
+      shadow="sm"
+    >
+      <CardHeader className="justify-center text-3xl font-bold">
+        로그인
+      </CardHeader>
+      <CardBody>
+        <div className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-y-2">
+            <Input type="email" label="Email" size="sm" radius="sm" />
+            <Input type="password" label="Password" size="sm" radius="sm" />
+          </div>
+          <Button
+            radius="sm"
+            size="lg"
+            className="bg-indigo-700 text-xl font-semibold text-white"
+          >
+            로그인
+          </Button>
+        </div>
+      </CardBody>
+      <CardFooter>
+        <div className="flex h-5 w-full justify-center space-x-4">
+          <Link href="/">아이디 찾기</Link>
+          <Divider orientation="vertical" className="h-full" />
+          <Link href="/">비밀번호 찾기</Link>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from '@/app/_components/login/LoginCard';
+
+export default function Login() {
+  return (
+    <div className="flex w-full justify-center">
+      <LoginCard />
+    </div>
+  );
+}
